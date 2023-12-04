be4bc9f1c60a096a161a1e362b4bd2ba3ce36e4a
Aleksandra Sanecka <a.saneckaa@gmail.com>
2023-12-04 11:44:53 +0100

    made main container component

---
+++ src/Container/index.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import Card from '../Card';
+
+const Container = () => {
+    return (
+        <div className='flex items-center justify-center min-h-screen bg-gray-200'>
+            <Card />
+        </div>
+    )
+}
+
+export default Container;
